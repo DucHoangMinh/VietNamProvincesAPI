71ab41506315be7f824ca47ba648abb0ed097b18
HoangMinhDuc <hoangminhduc4125@example.com>
2024-02-04 12:20:45 +0700

    change endpoints

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', (request, response) => {
 })
 
 app.get('/provinces', query.getAllProvinces)
-app.get('/district/:id', query.getDistrictInProvince)
-app.get('/ward/:id', query.getWardInDistrict)
+app.get('/districts/:id', query.getDistrictInProvince)
+app.get('/wards/:id', query.getWardInDistrict)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
